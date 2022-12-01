3428a52910f3b9f0c87bdcb996ad399571aeebec
Nana <nanaolomaiyani@gmail.com>
2022-12-01 13:56:09 +0300

    commit tree e0c4dc375a7503a579046012fc9c8b43cf8eabe8

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+    def create 
+        user = User.create!(user_params)
+        session[:user_id] = user.id 
+        render json: user, status: :created
+    end
+
+    def show 
+        user = User.find(session[:user_id])
+        render json: user, status: :created 
+    
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :password, :password_confirmation, :image_url, :bio)
+    end 
+end
